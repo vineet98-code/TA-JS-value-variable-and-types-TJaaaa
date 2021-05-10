fc4ede1899dba42cabb8f6a869f74379bc9d7a90
Vineet <vineet98love@gmail.com>
2021-05-10 17:42:30 +0530

    block-BJaaay

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,30 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+for(let i = 0; i < 11; i++){
+    console.log(i)
+ 
+}
 
 // Using `console.log` log all the evan values from 1 to 10.
+for(let i = 0; i < 11; i++){
+    if(i % 2 == 0){
+        console.log(i)
+    }
+}
 
 // Using `console.log` log all the od values from 1 to 10.
+for(let i = 0; i < 11; i++){
+    if(i % 2 !== 0){
+        console.log(i)
+    }
+}
 
 // Calculate the sum of all numbers from 1 to 10.
 
+
 // Log all the values from 1 to 10 using while loop
+let i = 0;
+while(i< 11){
+    i = i + 1;
+}
